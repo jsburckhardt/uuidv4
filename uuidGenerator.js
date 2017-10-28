f935ae16d43f8b01baf521b25afcafa782e6b178
juan <jsburckhardt@gmail.com>
2017-10-28 22:39:22 +1100

    working uuid generator

---
+++ uuidGenerator.js
@@ -0,0 +1,30 @@
+var baseConvert = require('baseconvert');
+
+function randomHexInc (low, high) {
+    let decimalNumber = Math.floor(Math.random() * (high - low + 1) + low);
+    let hexNumber = baseConvert.dec2hex(decimalNumber)
+    return hexNumber
+}
+
+let randomHexs=function(hexs,min,max){
+    let result = 0
+    for(let i =0;i<hexs;i++){
+        if(result == 0){
+            result=randomHexInc(min,max)
+        } else{
+            result=result+randomHexInc(min,max)
+        }        
+    }
+    return result
+}
+
+
+timeLow=randomHexs(8,0,15)               
+timeMid=randomHexs(4,0,15)               
+timeHighandVersion="4"+randomHexs(3,0,15)
+clockSeqandReserved=randomHexs(1,8,11)+randomHexs(1,0,15)   
+clockSeqLow=randomHexs(2,0,15)           
+item=randomHexs(12,0,15)                 
+
+console.log("uuid_v4: "+timeLow+'-'+timeMid+'-'+timeHighandVersion+'-'+clockSeqandReserved+clockSeqLow+'-'+item)
+
